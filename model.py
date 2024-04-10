8a0ab126d59dff37bc6407038651718f05a0a1f7
ren <renyabin@zto.com>
2024-04-10 17:16:29 +0800

    ccc

--- model.py
+++ model.py
@@ -183,7 +183,7 @@ class GPT(nn.Module):
         x = self.transformer.ln_f(x)
 
         if targets is not None:
-            class_weights = torch.tensor([0.20, 0.10, 0.03, 0.01, 0.03, 0.06, 0.06], dtype=torch.float).to(device)
+            # class_weights = torch.tensor([0.20, 0.10, 0.03, 0.01, 0.03, 0.06, 0.06], dtype=torch.float).to(device)
 
             # # Get the hidden state for the last position in the sequence
             # x_last = x[:, -1, :]
@@ -197,7 +197,7 @@ class GPT(nn.Module):
             # 计算所有时刻的loss
             # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
-            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1, weight=class_weights)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
